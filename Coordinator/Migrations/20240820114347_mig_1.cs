93c6fabf56490795b1e2cacbad0070b2770ceac1
Mikail Akar <huseyin.akar@bil.omu.edu.tr>
2024-08-20 16:15:48 +0300

    2PC

---
+++ Coordinator/Migrations/20240820114347_mig_1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Coordinator.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Nodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nodes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NodeStates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TransactionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsReady = table.Column<int>(type: "int", nullable: false),
+                    TransactionState = table.Column<int>(type: "int", nullable: false),
+                    NodeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NodeStates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NodeStates_Nodes_NodeId",
+                        column: x => x.NodeId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Nodes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("3ab08c9e-b209-49e5-96df-1315ce2bd18d"), "Payment.API" },
+                    { new Guid("68287d65-aef7-4bbf-9a9c-127756efe6ff"), "Order.API" },
+                    { new Guid("752805c2-386a-436e-bcd8-a7a4af8a0711"), "Stock.API" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NodeStates_NodeId",
+                table: "NodeStates",
+                column: "NodeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NodeStates");
+
+            migrationBuilder.DropTable(
+                name: "Nodes");
+        }
+    }
+}
